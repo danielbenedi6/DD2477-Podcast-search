1a501626f052c357a066bbfaef7035e89f7f4cf6
Daniel Benedí García <danielbenedigarcia@gmail.com>
2022-04-04 14:04:14 +0200

    Indexer finished

--- indexer.py
+++ indexer.py
@@ -1,8 +1,13 @@
 import csv
+import json
 from elasticsearch import Elasticsearch
 from itertools import chain
+import os
 
-FILE='podcasts-no-audio-13GB/spotify-podcasts-2020/metadata-summarization-testset.tsv'
+PATH='podcasts-no-audio-13GB/spotify-podcasts-2020/'
+FILE=PATH+'metadata-summarization-testset.tsv'
+TRANSCRIPTS=PATH+'podcasts-transcripts-summarization-testset'
+
 AUTH=("elastic",'')
 FINGERPRINT=''
 
@@ -11,7 +16,7 @@ es = Elasticsearch("https://localhost:9200",
                     ssl_assert_fingerprint=FINGERPRINT
                     )
 
-
+"""
 print("Parsing file...")
 data = list(chain.from_iterable([[{"index": {"_index":"spotify-podcasts"}}, row] for row in csv.DictReader(open(FILE, newline=''),delimiter='\t')]))
 
@@ -21,4 +26,41 @@ try:
 except:
     print(data)
 
+"""
+es.indices.refresh(index="spotify-podcasts")
 
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk(TRANSCRIPTS):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+for file in listOfFiles:
+    res = es.search(
+            index='spotify-podcasts', 
+            query = {
+                "match": {
+                    "episode_filename_prefix" : file.split('/')[-1].split('.')[0]
+                }
+            }
+        )
+
+    _id = res["hits"]["hits"][0]["_id"]
+    transcript = json.load(open(file))
+    text = ""
+    words = []
+    for chunk in transcript["results"]:
+        data = chunk["alternatives"][0]
+        if "transcript" in data and "words" in data:
+            text += data["transcript"]
+            words += data["words"]
+    
+    res = es.update(
+            index = 'spotify-podcasts',
+            id = _id,
+            doc = {
+                "transcript" : text,
+                "words" : words,
+                "raw" : transcript
+            }
+        )
+
+es.indices.refresh(index="spotify-podcasts")
